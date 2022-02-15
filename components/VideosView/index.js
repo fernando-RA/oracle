c454d02b87ec4c438ab829540621a88205c76fd6
frag 997 <fraguilar@pm.me>
2022-02-14 23:20:57 -0800

    refactoring

--- components/VideosView/index.js
+++ components/VideosView/index.js
@@ -1,46 +1,10 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { Tr, Td, IconButton, Box, Select, chakra } from "@chakra-ui/react";
-import { DataTable } from "components/GalleryLayout";
 import { Player } from "video-react";
 
-
-const TABLE_HEADERS = [
-  {
-    text: "Question",
-    props: {},
-  },
-  {
-    text: "Answer",
-    props: {},
-  },
-  {
-    text: "link to video",
-    props: {},
-  },
-];
-
-const Tweet = (props) => {
-  return (
-    <Tr>
-      <Td>{props.output.question}</Td>
-      <Td>{props.output.response}</Td>
-      <Td>
-        {
-          <Player
-            src={`https://sentientmachine.online/${props.output.video}`}
-          />
-        }
-      </Td>
-    </Tr>
-  );
-};
-
-const TweetsView = ({ items }) => {
-  const { query } = useRouter();
+const VideosView = ({ items }) => {
   return items ? (
-    <Box>
-      <chakra.h2 fontSize={24}>All creations</chakra.h2>
+    <div>
+      <h2 fontSize={24}>All creations</h2>
       <div className="grid-container">
         {items
           .slice(0)
@@ -48,21 +12,21 @@ const TweetsView = ({ items }) => {
           .map((item, index) => {
             return item.status.status === "complete" ? (
               <div className="grid-item" key={index}>
-                <div>{item.output.question}</div>
-                <div>{item.output.response}</div>
-                <div>
+                <div className="player">
                   {
                     <Player
                       src={`https://sentientmachine.online/${item.output.video}`}
                     />
                   }
                 </div>
+                <h3>{item.output.question}</h3>
+                <p>{item.output.response}</p>
               </div>
             ) : null;
           })}
       </div>
-    </Box>
+    </div>
   ) : null;
 };
 
-export default TweetsView;
+export default VideosView;
